b58d50f69f6300641d1218a8b2989ec33b6497d7
i-bu-hub <114901868+i-bu-hub@users.noreply.github.com>
2024-01-31 16:39:32 +0900

    Add files via upload

---
+++ ホームページ/main.js
@@ -0,0 +1,19 @@
+document.getElementById('proposal').onclick = function() {
+  window.location.href = 'dc2023_10.pdf';
+};
+
+document.getElementById('use').onclick = function() {
+  window.location.href = 'manual.html';
+};
+
+document.getElementById('gitHub').onclick = function() {
+  window.location.href = 'https://github.com/ikedatomoki/digital-application';
+};
+
+document.getElementById('intro').onclick = function() {
+  window.location.href = 'https://youtu.be/zdMvpjwSAMQ';
+};
+
+document.getElementById('download').onclick = function() {
+  window.location.href = '収支管理アプリ.zip';
+};
